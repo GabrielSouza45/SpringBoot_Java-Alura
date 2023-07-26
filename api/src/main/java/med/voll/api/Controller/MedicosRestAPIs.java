ea674b8cb7887f80452c3fa80345a66da85e24e7
Gabriel Freitas Souza <gabrielsouza45@live.com>
2023-07-26 16:47:48 -0300

    Atualização classe Controller

--- api/src/main/java/med/voll/api/Controller/MedicosRestAPIs.java
+++ api/src/main/java/med/voll/api/Controller/MedicosRestAPIs.java
@@ -1,9 +1,10 @@
 package med.voll.api.Controller;
 
-import med.voll.api.Record.Medico;
+import jakarta.transaction.Transactional;
+import med.voll.api.Record.MedicoDTO;
 import med.voll.api.Repository.MedicoRepository;
+import med.voll.api.model.Medico;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicosRestAPIs {
 
 
-
+    @Autowired
+    private MedicoRepository medicoRepository;
 
     @PostMapping(value = "/adicionar")
     @CrossOrigin(allowedHeaders = "*", origins = "*")
-    public void cadastrarMedico(Medico medico){
+    @Transactional
+    public void cadastrarMedico(MedicoDTO medico){
 
-
+        medicoRepository.save(new Medico(medico));
     }
 
 }
