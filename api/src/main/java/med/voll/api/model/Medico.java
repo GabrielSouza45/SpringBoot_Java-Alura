5805a9c6a7b8d7b6fc0c5d58d0c4358c43a3dc2a
Gabriel Freitas Souza <gabrielsouza45@live.com>
2023-07-26 16:46:35 -0300

    Criação das classes model

---
+++ api/src/main/java/med/voll/api/model/Medico.java
@@ -0,0 +1,37 @@
+package med.voll.api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import med.voll.api.Record.MedicoDTO;
+import med.voll.api.Tipo.Especialidade;
+
+@Table(name = "medico")
+@Entity(name = "Medico")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Medico(MedicoDTO medico) {
+        this.nome = medico.nome();
+        this.email = medico.email();
+        this.crm = medico.crm();
+        this.especialidade = medico.especialidade();
+        this.endereco = new Endereco(medico.endereco());
+    }
+}
