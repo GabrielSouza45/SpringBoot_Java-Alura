bb475ababdc6bc72595f45801dc11a527c4d56d4
Gabriel Freitas Souza <gabrielsouza45@live.com>
2023-07-26 16:48:05 -0300

    Criação da classe Repository

--- api/src/main/java/med/voll/api/Repository/MedicoRepository.java
+++ api/src/main/java/med/voll/api/Repository/MedicoRepository.java
@@ -1,4 +1,9 @@
 package med.voll.api.Repository;
 
-public interface MedicoRepository {
+import med.voll.api.model.Medico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
 }
