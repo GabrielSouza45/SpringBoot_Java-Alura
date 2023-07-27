fa0c9698f2152d19cb6a45b3b07cb5f4b60ac9d4
Gabriel Freitas Souza <gabrielsouza45@live.com>
2023-07-27 11:02:55 -0300

    Listagem médicos

--- api/src/main/java/med/voll/api/Controller/MedicosRestAPIs.java
+++ api/src/main/java/med/voll/api/Controller/MedicosRestAPIs.java
@@ -2,11 +2,14 @@ package med.voll.api.Controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.Record.ListagemMedicoDTO;
 import med.voll.api.Record.MedicoDTO;
 import med.voll.api.Repository.MedicoRepository;
 import med.voll.api.model.Medico;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/medicos")
@@ -17,13 +20,18 @@ public class MedicosRestAPIs {
     @Autowired
     private MedicoRepository medicoRepository;
 
-    @PostMapping(value = "/adicionar")
+    @PostMapping
     @CrossOrigin(allowedHeaders = "*", origins = "*")
     @Transactional
     public void cadastrarMedico(@RequestBody @Valid MedicoDTO medico){
 
         medicoRepository.save(new Medico(medico));
 
+    }
+
+    @GetMapping
+    public List<ListagemMedicoDTO> listar() {
+        return medicoRepository.findAll().stream().map(ListagemMedicoDTO::new).toList();
     }
 
 }
---
+++ api/src/main/java/med/voll/api/Record/ListagemMedicoDTO.java
@@ -0,0 +1,20 @@
+package med.voll.api.Record;
+
+import med.voll.api.Tipo.Especialidade;
+import med.voll.api.model.Medico;
+
+public record ListagemMedicoDTO(
+        String nome,
+        String email,
+        String crm,
+        Especialidade especialidade
+) {
+    public ListagemMedicoDTO(Medico medico) {
+        this(
+                medico.getNome(),
+                medico.getEmail(),
+                medico.getCrm(),
+                medico.getEspecialidade()
+        );
+    }
+}
