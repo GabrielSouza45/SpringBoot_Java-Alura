d51a0b94380a214e1ddf32bd4983ddfd5edae9da
Gabriel Freitas Souza <gabrielsouza45@live.com>
2023-09-15 10:49:35 -0300

    Adicionado mensagem de erro as anotações de validation, forma 2

--- api/src/main/java/med/voll/api/Record/Paciente/PacienteDTO.java
+++ api/src/main/java/med/voll/api/Record/Paciente/PacienteDTO.java
@@ -1,20 +1,22 @@
 package med.voll.api.Record.Paciente;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import med.voll.api.Record.Endereco.EnderecoDTO;
 
 public record PacienteDTO(
-        @NotBlank(message = "Nome é obrigatório")
+        @NotBlank(message = "{nome.obrigatorio}")
         String nome,
-        @NotBlank(message = "Email é obrigatório")
+        @NotBlank(message = "{email.obrigatorio}")
+        @Email(message = "{email.invalido}")
         String email,
-        @NotBlank(message = "Telefone é obrigatório")
+        @NotBlank(message = "{telefone.obrigatorio}")
         String telefone,
-        @NotBlank(message = "CPF é obrigatório (11 dígitos)")
-        @Pattern(regexp = "\\d{11}")
+        @NotBlank(message = "{crm.obrigatorio}")
+        @Pattern(regexp = "\\d{11}", message = "{crm.invalido}")
         String cpf,
         @NotNull(message = "Endereço é obrigatório")
         @Valid
