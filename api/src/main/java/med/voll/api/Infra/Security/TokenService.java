ac5bc343c2418edd81d11720e3b14162d09a378b
Gabriel Freitas Souza <gabrielsouza45@live.com>
2023-09-22 16:12:32 -0300

    Retorno e validacao de token

--- api/src/main/java/med/voll/api/Infra/Security/TokenService.java
+++ api/src/main/java/med/voll/api/Infra/Security/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.api.Infra.Security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.Model.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -33,6 +34,22 @@ public class TokenService {
             throw new RuntimeException("Erro ao gerar token jwt", exception);
         }
 
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            Algorithm algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    // specify an specific claim validations
+                    .withIssuer("API Voll.med")
+                    // reusable verifier instance
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token inválido ou expirado.");
+        }
     }
 
     private Instant dataExpiracao() {
